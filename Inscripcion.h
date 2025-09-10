22ef70ed16e98d3da3612a3a73005f60c77ac0d9
Gaspar Navarro <gaspar.navarro@alumnos.ucn.cl>
2025-09-10 18:06:04 -0300

    clase para la inscripcion  agregada

---
+++ Inscripcion.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Alumno.h"
+#include "Curso.h"
+#include "ListaNotas.h"
+
+class Inscripcion{
+    private :
+        Alumno* alumno;
+        Curso* curso;
+        ListaNotas* notas;
+    public: 
+        Inscripcion(Alumno*, Curso*);
+        void agregarNota(float);
+        float calcularPromedio();
+
+        Alumno* getAlumno();
+        Curso* getCurso();
+        int getCantidadNotas();
+        
+};
